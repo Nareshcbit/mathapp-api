ae23274f7aee03ad49094a8550e22983cf552c2d
Naresh Madiraju <naresh.johnny@gmail.com>
2023-08-13 18:33:35 -0400

    delete .aws-sam

--- .aws-sam/cache/97e05edc-fb45-47a8-a84c-4e0f2954a17a/routes/question_routes.py
+++
@@ -1,6 +0,0 @@
-# src/routes/question_routes.py
-
-from controllers import question_controller
-
-def get(event):
-    return question_controller.get_questions(event)
--- .aws-sam/cache/97e05edc-fb45-47a8-a84c-4e0f2954a17a/routes/topic_routes.py
+++
@@ -1,6 +0,0 @@
-# src/routes/topic_routes.py
-
-from controllers import topic_controller
-
-def get(event):
-    return topic_controller.get_topics(event)
